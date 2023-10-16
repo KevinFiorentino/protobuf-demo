2f80c08312839a9f956854631321af6a0dc4a1c0
Kevin <kevinfiorentino.1294@gmail.com>
2023-10-15 23:48:15 -0300

    Comments

--- index.js
+++ index.js
@@ -2,26 +2,25 @@ const grpc = require('grpc');
 const path = require('path');
 const protoLoader = require('@grpc/proto-loader');
 
-// Cargar las definiciones generadas
 const PROTO_PATH = path.resolve(__dirname, 'data_explorer.proto');
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, { keepCase: true, longs: String, enums: String, defaults: true, oneofs: true });
 const dataExplorerProto = grpc.loadPackageDefinition(packageDefinition);
 
-// Dirección y puerto del servidor gRPC
+// gRPC server
 const serverAddress = 'https://dev.heliotics.com';
 
-// Crear un cliente gRPC
+// gRPC cliente
 const client = new dataExplorerProto.DataExplorerGrpcService(serverAddress, grpc.credentials.createInsecure());
 
-// Crear una solicitud para GetSensorDataStream
+// GetSensorDataStream request
 const getSensorDataRequest = {
   sensor_id: '17d88eca-5bbb-49e9-9da3-3329f0358059',
 };
 
-// Llamar al método GetSensorDataStream del servicio
+// Call GetSensorDataStream
 const call = client.GetSensorDataStream(getSensorDataRequest);
 
-// Manejar eventos de la transmisión
+// Events
 call.on('data', (sensorData) => {
   console.log('Received SensorData:', sensorData);
 });
